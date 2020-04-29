93b4b3a38f02470831616a086bd8b73419881c5c
ajperez <ajperezmontilla@gmail.com>
2020-04-29 13:12:13 +0200

    Updated with required changes
    - I added the instaceprofile with the  UdacityS3ReadOnlyEC2 role and attached to the launch configuration.
    - In the script crear.sh, I added the stack capabilitity to assign IAM resources to the AWS account

--- crear-stack.sh
+++ crear-stack.sh
@@ -4,6 +4,8 @@ aws cloudformation create-stack
 -- stack-name $1 /
 -- template-body file://$2 /
 -- parameters file://$3 /
--- region=us-west-1
+-- region=us-west-1 /
+-- capabilities CAPABILITY_NAMED_IAM
+
 
 
